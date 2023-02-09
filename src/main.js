d489a7ec7bc762947b3db5ddfedd83ed0adfe091
yuzi <1933254975@qq.com>
2023-02-09 11:03:07 +0800

    oos前端项目

--- src/main.js
+++ src/main.js
@@ -1,10 +1,93 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import {postRequest} from "@/utils/api";
+import {putRequest} from "@/utils/api";
+import {getRequest} from "@/utils/api";
+import {deleteRequest} from "@/utils/api";
+import {uploadImgRequest} from "@/utils/api";
+import 'font-awesome/css/font-awesome.css';
+import {downLoadRequest} from "@/utils/download";
+import {initMenu} from "@/utils/menus";
+import UserInfo from "@/views/UserInfo";
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {size: 'small'})
+
+//插件形式使用请求
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.downLoadRequest = downLoadRequest;
+Vue.prototype.uploadImgRequest = uploadImgRequest;
+
+router.beforeEach((to, from, next) => {
+    // if (window.sessionStorage.getItem('tokenStr')) {
+    //     initMenu(router,store);
+    //     //判断用户信息是否存在
+    //     if (!window.sessionStorage.getItem('user')) {
+    //         return getRequest('/admin/info').then(resp => {
+    //             if (resp) {
+    //                 //存入用户信息
+    //                 window.sessionStorage.setItem('user', JSON.stringify(resp));
+    //                 store.commit('INIT_ADMIN', resp);
+    //                 next();
+    //             }
+    //         })
+    //     }
+    //     next();
+    // } else {
+    //     if (to.path == '/') {
+    //         next();
+    //     } else {
+    //         next('/?redirect=' + to.path);
+    //     }
+    //     next();
+    // }
+
+    // alert("00")
+    if (window.sessionStorage.getItem('tokenStr')) {
+        initMenu(router, store);
+        if (!window.sessionStorage.getItem('user')) {
+            //连接websocket
+            console.log('连接websocket');
+            store.dispatch('connect');
+            return getRequest('/user/info').then(resp=>{
+                if(resp){
+                    window.sessionStorage.setItem('user', JSON.stringify(resp));
+                    store.commit('INIT_ADMIN', resp);
+                    // alert("你倒是next啊1")
+                    next('/home');
+                }
+            })
+        }else {
+            console.log(JSON.parse(window.sessionStorage.getItem('user')));
+            console.log(store.state.currentAdmin);
+            store.commit('INIT_ADMIN', JSON.parse(window.sessionStorage.getItem('user')));
+            console.log(store.state.currentAdmin);
+
+        }
+
+        // alert("你倒是next啊3")
+        next();
+    }
+    else {
+        if (to.path == '/') {
+            // alert("你倒是next啊4")
+            next();
+        } else {
+            // alert("你倒是next啊4")
+            next('/?redirect=' + to.path);
+        }
+    }
+})
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,232 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login'
+import Home from "@/views/Home";
+import Test1 from "@/views/Test1";
+import Test2 from "@/views/Test2";
+import EmpBasic from "@/views/emp/EmpBasic";
+import EmpAdv from "@/views/emp/EmpAdv";
+import PerEc from "@/views/per/PerEc";
+import PerEmp from "@/views/per/PerEmp";
+import PerMv from "@/views/per/PerMv";
+import PerSalary from "@/views/per/PerSalary";
+import PerTrain from "@/views/per/PerTrain";
+import SalSob from "@/views/sal/SalSob";
+import SalSobCfg from "@/views/sal/SalSobCfg";
+import SalTable from "@/views/sal/SalTable";
+import SalMonth from "@/views/sal/SalMonth";
+import SalSearch from "@/views/sal/SalSearch";
+import StaAll from "@/views/sta/StaAll";
+import StaPers from "@/views/sta/StaPers";
+import StaScore from "@/views/sta/StaScore";
+import StaRecord from "@/views/sta/StaRecord";
+import SysBasic from "@/views/sys/SysBasic";
+import SysConfig from "@/views/sys/SysConfig";
+import SysLog from "@/views/sys/SysLog";
+import SysAdmin from "@/views/sys/SysAdmin";
+import SysData from "@/views/sys/SysData";
+import SysInit from "@/views/sys/SysInit";
+import FriendChat from "@/views/chat/FriendChat";
+import UserInfo from "@/views/UserInfo";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+// 解决编程式路由往同一地址跳转时会报错的情况
+const originalPush = VueRouter.prototype.push;
+const originalReplace = VueRouter.prototype.replace;
+
+// push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+    if (onResolve || onReject)
+        return originalPush.call(this, location, onResolve, onReject);
+    return originalPush.call(this, location).catch(err => err);
+};
+
+//replace
+VueRouter.prototype.replace = function push(location, onResolve, onReject) {
+    if (onResolve || onReject)
+        return originalReplace.call(this, location, onResolve, onReject);
+    return originalReplace.call(this, location).catch(err => err);
+};
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'Login',
+        component: Login,
+        hidden: true
+    },
+    // {
+    //     path: '/home',
+    //     name: '员工资料',
+    //     iconCls: 'fa fa-user-circle-o',
+    //     component: Home,
+    //     children: [
+    //         {
+    //             path: '/EmpBasic',
+    //             name: '基本资料',
+    //             component: EmpBasic
+    //         },
+    //         {
+    //             path: '/EmpAdv',
+    //             name: '高级资料',
+    //             component: EmpAdv
+    //         }
+    //     ]
+    // },
+    // {
+    //     path: '/home',
+    //     name: '人事管理',
+    //     iconCls: 'fa fa-address-card-o',
+    //     component: Home,
+    //     children: [
+    //         {
+    //             path: '/PerEc',
+    //             name: '员工奖惩',
+    //             component: PerEc
+    //         },
+    //         {
+    //             path: '/PerEmp',
+    //             name: '员工资料',
+    //             component: PerEmp
+    //         },
+    //         {
+    //             path: '/PerMv',
+    //             name: '员工调动',
+    //             component: PerMv
+    //         },
+    //         {
+    //             path: '/PerSalary',
+    //             name: '员工调薪',
+    //             component: PerSalary
+    //         },
+    //         {
+    //             path: '/PerTrain',
+    //             name: '员工培训',
+    //             component: PerTrain
+    //         }
+    //     ]
+    // },
+    // {
+    //     path: '/home',
+    //     name: '薪资管理',
+    //     iconCls: 'fa fa-money',
+    //     component: Home,
+    //     children: [
+    //         {
+    //             path: '/SalSob',
+    //             name: '工资账套管理',
+    //             component: SalSob
+    //         },
+    //         {
+    //             path: '/SalSobCfg',
+    //             name: '员工账套管理',
+    //             component: SalSobCfg
+    //         },
+    //         {
+    //             path: '/SalTable',
+    //             name: '工资表管理',
+    //             component: SalTable
+    //         },
+    //         {
+    //             path: '/SalMonth',
+    //             name: '月末处理',
+    //             component: SalMonth
+    //         },
+    //         {
+    //             path: '/SalSearch',
+    //             name: '工资表查询',
+    //             component: SalSearch
+    //         }
+    //     ]
+    // },
+    // {
+    //     path: '/home',
+    //     name: '统计管理',
+    //     iconCls: 'fa fa-bar-chart',
+    //     component: Home,
+    //     children: [
+    //         {
+    //             path: '/StaAll',
+    //             name: '综合信息统计',
+    //             component: StaAll
+    //         },
+    //         {
+    //             path: '/StaScore',
+    //             name: '员工积分统计',
+    //             component: StaScore
+    //         },
+    //         {
+    //             path: '/StaPers',
+    //             name: '人事信息统计',
+    //             component: StaPers
+    //         },
+    //         {
+    //             path: '/StaRecord',
+    //             name: '人事记录统计',
+    //             component: StaRecord
+    //         }
+    //     ]
+    // },
+    // {
+    //     path: '/home',
+    //     name: '系统管理',
+    //     iconCls: 'fa fa-windows',
+    //     component: Home,
+    //     children: [
+    //         {
+    //             path: '/SysBasic',
+    //             name: '基础信息设置',
+    //             component: SysBasic
+    //         },
+    //         {
+    //             path: '/SysConfig',
+    //             name: '系统管理',
+    //             component: SysConfig
+    //         },
+    //         {
+    //             path: '/SysLog',
+    //             name: '操作日志管理',
+    //             component: SysLog
+    //         },
+    //         {
+    //             path: '/SysAdmin',
+    //             name: '操作员管理',
+    //             component: SysAdmin
+    //         },
+    //         {
+    //             path: '/SysData',
+    //             name: '备份恢复数据库',
+    //             component: SysData
+    //         },
+    //         {
+    //             path: '/SysInit',
+    //             name: '初始化数据库',
+    //             component: SysInit
+    //         }
+    //     ]
+    // },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        hidden: true,
+        children: [
+            {
+                path: '/chat',
+                name: '在线聊天',
+                component: FriendChat
+            },
+            {
+                path: '/userinfo',
+                name: '个人中心',
+                component: UserInfo
+            }
+        ]
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
